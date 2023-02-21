3c43e82354adea3db9f78b49edbc52040668ab5a
Adryann Layon <adryann.layon7@gmail.com>
2023-02-21 19:34:36 -0300

    started using styled components

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,10 @@
+import { Container } from './styles'
+
 export function Details() {
 
   return (
-    <>
+    <Container>
       <h1>Hello World!</h1>
-      <p>Olá Adryann</p>
-    </>
+    </Container>
   )
 }
