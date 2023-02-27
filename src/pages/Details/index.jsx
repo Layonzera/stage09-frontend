43fc546b6bc191deda5fe2001ee496ef11cd96ed
Adryann Layon <adryann.layon7@gmail.com>
2023-02-27 14:13:50 -0300

    add: ButtonText component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,14 +2,19 @@ import { Container, Links } from './styles'
 
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
+import { ButtonText } from '../../components/ButtonText'
 import { Section } from '../../components/Section'
 import { Tag } from '../../components/Tag'
+
 
 export function Details() {
 
   return (
     <Container>
       <Header />
+      
+      <ButtonText title="Excluir nota" />
+
       <Section title="Links úteis">
         <Links>
           <li>
