aac04f04cfe33ba005d9d237faa38deafefe732b
Adryann Layon <adryann.layon7@gmail.com>
2023-02-28 14:59:53 -0300

    Button create note finished

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,5 @@
+import { FiPlus } from 'react-icons/fi'
+
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles.js'
 
 import { Header } from '../../components/Header'
@@ -22,7 +24,10 @@ export function Home() {
 
       <Content></Content>
 
-      <NewNote></NewNote>
+      <NewNote>
+        <FiPlus />
+        Criar Nota
+      </NewNote>
     </Container>
   )
 }
